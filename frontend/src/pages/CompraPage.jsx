3b6173e654499d9ed21b449eff7604accf6cb627
Jluis071 <joseluisvm07@gmail.com>
2024-11-10 20:48:21 -0500

    CompraPage Funcional

--- frontend/src/pages/CompraPage.jsx
+++ frontend/src/pages/CompraPage.jsx
@@ -1,6 +1,95 @@
 import { useState } from "react"
 
 const CompraPage = () => {
+    const [nombre, setNombre] = useState("")
+    const [direccion, setDireccion] = useState("")
+    const [celular, setCelular] = useState("")
+    const [dni, setDni] = useState("")
+    const [pagoId, setPagoId] = useState(1)
+    
+    const metodoPago = [
+        {id: 1, nombre: "Tarjeta de credito/debito"},
+        {id: 2, nombre: "Paypal"}
+    ]
 
+    const handleGuardar = () => {
+        const datosComprador = {
+            nombre,
+            direccion,
+            celular,
+            dni,
+            pagoId,
+        };
+        console.log("Datos guardados:", datosComprador);
+    };
+
+    return(
+    <div>
+        <h1> DATOS DEL COMPRADOR</h1>
+        <form>
+        <div>
+            <label>Nombre:</label>
+            <input
+            type="text"
+            name="nombre"
+            valor={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+            />
+        </div>
+
+        <div>
+            <label>Dirección:</label>
+            <input
+            type="text"
+            name="direccion"
+            valor={direccion}
+            onChange={(e) => setDireccion(e.target.value)}
+            />
+        </div>
+
+        <div>
+            <label>Celular:</label>
+            <input
+            type="text"
+            name="celular"
+            valor={celular}
+            onChange={(e) => setCelular(e.target.value)}
+            />
+        </div>
+
+        <div>
+            <label>DNI:</label>
+            <input
+            type="text"
+            name="dni"
+            valor={dni}
+            onChange={(e) => setDni(e.target.value)}
+            />
+        </div>
+
+        <div>
+            <label>Método de Pago:</label>
+            <select
+                name="metodoPago"
+                value={pagoId}
+                onChange={(e) => setPagoId(Number(e.target.value))}
+            >
+                    <option value="">Selecciona un método</option>
+                    {metodoPago.map((metodo) => (
+                        <option key={metodo.id} value={metodo.id}>
+                            {metodo.nombre}
+                        </option>
+                    ))}
+            </select>
+        </div>
+       
+        <button type="button" onClick={handleGuardar}>
+                    Guardar
+                </button>
+                
+        </form>
+                
+    </div>
+    )
 }
 export default CompraPage
--- frontend/src/pages/DetallesPage.jsx
+++ frontend/src/pages/DetallesPage.jsx
@@ -1,8 +1,73 @@
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const DetallesPage = () => {
+    const [nombre, setNombre] = useState("Producto");
+    const [precio, setPrecio] = useState(100);
+    const [descripcion, setDescripcion] = useState("Descripción o Características del Producto");
+    const [cantidad, setCantidad] = useState(1);
 
-        };
+    const navigate = useNavigate();
 
-export default DetallesPage
+
+    const [quantity, setQuantity] = useState(1);
+
+    // Función para cambiar la cantidad del producto
+    const changeQuantity = (change) => {
+        setQuantity((prevQuantity) => {
+            const newQuantity = prevQuantity + change;
+            return newQuantity < 1 ? 1 : newQuantity; // No permitir menos de 1
+        });
+    };
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <div className="card" style={{ width: '18rem' }}>
+                        <img src="https://via.placeholder.com/400" alt={nombre} />
+                        <div className="card-body">
+                            <h5 className="card-title">{nombre}</h5>
+                            <p>Precio: ${precio}</p>
+                            <p className="card-text">{descripcion}</p>
+                        </div>
+                        <div>
+                            <div className="input-group quantity-input w-auto">
+                                <button
+                                    className="btn btn-outline-secondary"
+                                    type="button"
+                                    onClick={() => changeQuantity(-1)}
+                                >
+                                    -
+                                </button>
+                                <input
+                                    type="number"
+                                    className="form-control text-center"
+                                    value={quantity}
+                                    readOnly
+                                    min="1"
+                                    style={{ maxWidth: '60px' }}
+                                />
+                                <button
+                                    className="btn btn-outline-secondary"
+                                    type="button"
+                                    onClick={() => changeQuantity(1)}
+                                >
+                                    +
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <button className="btn btn-primary w-100 p-2 mx-2" onClick={() => navigate('/compra')}>
+                        Agregar al Carrito
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DetallesPage;
