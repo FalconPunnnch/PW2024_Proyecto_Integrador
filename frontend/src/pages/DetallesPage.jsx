c0cb7b892d14a7edc8f461a2e94c22459afe5db1
Jluis071 <joseluisvm07@gmail.com>
2024-11-10 21:10:04 -0500

    Detalles

--- frontend/src/pages/DetallesPage.jsx
+++ frontend/src/pages/DetallesPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+
 const DetallesPage = () => {
     const [nombre, setNombre] = useState("Producto");
     const [precio, setPrecio] = useState(100);
@@ -23,50 +24,51 @@ const DetallesPage = () => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col">
-                    <div className="card" style={{ width: '18rem' }}>
-                        <img src="https://via.placeholder.com/400" alt={nombre} />
-                        <div className="card-body">
-                            <h5 className="card-title">{nombre}</h5>
-                            <p>Precio: ${precio}</p>
-                            <p className="card-text">{descripcion}</p>
+                <div className="col-4">
+                    <img src="https://via.placeholder.com/400" alt={nombre} ></img>
+                </div>
+                <div className="col-8">
+                    <h2>{nombre}</h2>
+                    <p>Precio: ${precio}</p>
+                    <p>{descripcion}</p>
+                    <div className="row">
+                        <div className="input-group quantity-input w-auto col-4">
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={() => changeQuantity(-1)}
+                            >
+                                -
+                            </button>
+                            <input
+                                type="number"
+                                className="form-control text-center"
+                                value={quantity}
+                                readOnly
+                                min="1"
+                                style={{ maxWidth: '60px' }}
+                            />
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={() => changeQuantity(1)}
+                            >
+                                +
+                            </button>
+
                         </div>
-                        <div>
-                            <div className="input-group quantity-input w-auto">
-                                <button
-                                    className="btn btn-outline-secondary"
-                                    type="button"
-                                    onClick={() => changeQuantity(-1)}
-                                >
-                                    -
-                                </button>
-                                <input
-                                    type="number"
-                                    className="form-control text-center"
-                                    value={quantity}
-                                    readOnly
-                                    min="1"
-                                    style={{ maxWidth: '60px' }}
-                                />
-                                <button
-                                    className="btn btn-outline-secondary"
-                                    type="button"
-                                    onClick={() => changeQuantity(1)}
-                                >
-                                    +
-                                </button>
-                            </div>
+                        <div className="col-4">
+                            <button className="btn btn-primary w-100 p-2 mx-2" onClick={() => navigate('/compra')}>
+                                Agregar al Carrito
+                            </button>
                         </div>
                     </div>
 
-
-
-                    <button className="btn btn-primary w-100 p-2 mx-2" onClick={() => navigate('/compra')}>
-                        Agregar al Carrito
-                    </button>
                 </div>
+
             </div>
         </div>
+
     );
 };
 
